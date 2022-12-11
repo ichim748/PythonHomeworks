cf8198cbb20e67c3fcc3e9d08745a8454a123a55
Ichim Cosmin-Stefan <73492909+ichim748@users.noreply.github.com>
2022-12-11 15:54:51 +0200

    Mouse can move
    Added the ability for the mouse to move

--- PythonFinalProject/main.py
+++ PythonFinalProject/main.py
@@ -1,4 +1,7 @@
+import random
 import sys
+import time
+
 import pygame
 
 pygame.init()
@@ -8,6 +11,9 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 background = pygame.image.load('background_photo.jpg')
+moved = [False]
+blocked_tiles = [0]
+mouse_moves = [0]
 
 
 class Tile(pygame.sprite.Sprite):
@@ -45,6 +51,8 @@ class Tile(pygame.sprite.Sprite):
     def check_click(self, mouse):
         if self.rect.collidepoint(mouse) and (self.tip == 0 or self.tip == 1):
             self.tip = 2
+            moved[0] = True
+            blocked_tiles[0] += 1
 
     def check_hover(self, mouse):
         if self.rect.collidepoint(mouse):
@@ -56,12 +64,12 @@ class Tile(pygame.sprite.Sprite):
 
 def grid_position_to_coordinates(x, y):
     if x % 2 == 0:
-        a = 200 + y * 55
-        b = 27 + x * 45
+        a = 173 + y * 56
+        b = 27 + x * 53
         return a, b
     else:
-        a = 227 + y * 55
-        b = 27 + x * 45
+        a = 200 + y * 56
+        b = 27 + x * 53
         return a, b
 
 
@@ -73,7 +81,7 @@ for i in range(11):
     for j in range(11):
         coordinates = grid_position_to_coordinates(i, j)
         if i == 5 and j == 5:
-            mouse_position = (i, j)
+            mouse_position = [i, j]
             tile = Tile(3, coordinates[0], coordinates[1], i, j)
             tile_group.add(tile)
             temp.append(tile)
@@ -99,6 +107,21 @@ def player_won():
             return True
 
 
+def mouse_move():
+    found = False
+    while not found:
+        optiune_x = random.randint(-1, 1)
+        optiune_y = random.randint(-1, 1)
+        if (optiune_x, optiune_y) in [(0, -1), (-1, 0), (1, 0), (0, 1)]:
+            if tile_matrix[mouse_position[0]+optiune_x][mouse_position[1]+optiune_y].tip != 2:
+                if not found:
+                    found = True
+                    tile_matrix[mouse_position[0]][mouse_position[1]].tip = 0
+                    tile_matrix[mouse_position[0] + optiune_x][mouse_position[1] + optiune_y].tip = 3
+                    mouse_position[0] += optiune_x
+                    mouse_position[1] += optiune_y
+
+
 while not mouse_won() and not player_won():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -111,8 +134,18 @@ while not mouse_won() and not player_won():
             for i in tile_group:
                 i.check_hover(event.pos)
 
+    if not player_won() and moved[0] and blocked_tiles[0] > mouse_moves[0]:
+        moved[0] = False
+        mouse_moves[0] += 1
+        mouse_move()
+
     pygame.display.flip()
     screen.blit(background, (0, 0))
     tile_group.draw(screen)
     tile_group.update()
     clock.tick(60)
+else:
+    if mouse_won():
+        print('The mouse won')
+    else:
+        print('The player won')

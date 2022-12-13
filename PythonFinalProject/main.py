a82fbf767e00d3c7739b3aa68fbf3dca942d1b2d
Ichim Cosmin-Stefan <73492909+ichim748@users.noreply.github.com>
2022-12-13 15:13:01 +0200

    Added ability to play against human, also 3 computer difficulties

--- PythonFinalProject/main.py
+++ PythonFinalProject/main.py
@@ -52,12 +52,53 @@ class Tile(pygame.sprite.Sprite):
             moved[0] = True
             blocked_tiles[0] += 1
 
+    def check_click_with_turn(self, mouse, turn, tiles):
+        if turn[0] == 0:
+            if self.rect.collidepoint(mouse) and (self.tip == 0 or self.tip == 1):
+                self.tip = 2
+                moved[0] = True
+                blocked_tiles[0] += 1
+                turn[0] = 1
+        else:
+            if self.rect.collidepoint(mouse) and (self.tip == 0 or self.tip == 1):
+                if mouse_position[0] % 2 == 1:
+                    possible_moves = [(-1, 0), (0, -1), (1, 0), (-1, 1), (0, 1), (1, 1)]
+                else:
+                    possible_moves = [(-1, -1), (0, -1), (1, -1), (1, 0), (0, 1), (-1, 0)]
+                for t in possible_moves:
+                    if self.i == mouse_position[0] + t[0] and self.j == mouse_position[1] + t[1]:
+                        tiles[mouse_position[0]][mouse_position[1]].tip = 0
+                        self.tip = 3
+                        mouse_position[0] = self.i
+                        mouse_position[1] = self.j
+                        break
+                turn[0] = 0
+
     def check_hover(self, mouse):
         if self.rect.collidepoint(mouse):
             if self.tip == 0 or self.tip == 1:
                 self.tip = 1
         elif self.tip == 1:
             self.tip = 0
+
+    def check_hover_with_turn(self, mouse, rand):
+        if rand[0] == 0:
+            if self.rect.collidepoint(mouse):
+                if self.tip == 0 or self.tip == 1:
+                    self.tip = 1
+            elif self.tip == 1:
+                self.tip = 0
+        else:
+            if self.rect.collidepoint(mouse) and (self.tip == 0 or self.tip == 1):
+                if mouse_position[0] % 2 == 1:
+                    possible_moves = [(-1, 0), (0, -1), (1, 0), (-1, 1), (0, 1), (1, 1)]
+                else:
+                    possible_moves = [(-1, -1), (0, -1), (1, -1), (1, 0), (0, 1), (-1, 0)]
+                for t in possible_moves:
+                    if self.i == mouse_position[0] + t[0] and self.j == mouse_position[1] + t[1]:
+                        self.tip = 1
+            elif self.tip == 1:
+                self.tip = 0
 
 
 def grid_position_to_coordinates(x, y):
@@ -88,6 +129,7 @@ for i in range(11):
             tile_group.add(tile)
             temp.append(tile)
     tile_matrix.append(temp)
+
 # Dupa ce am generat tabla trebuie sa adaugam niste blocked tiles (asa era in jocul original)
 number = random.randint(5, 10)
 impossible_positions = [(5, 5)]
@@ -189,31 +231,67 @@ def mouse_move_using_dijkstra():
     mouse_position[1] = path[len(path)-2].j
 
 
-if __name__ == '__main__': 
-    while not mouse_won() and not player_won():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                for i in tile_group:
-                    i.check_click(event.pos)
-            elif event.type == pygame.MOUSEMOTION:
-                for i in tile_group:
-                    i.check_hover(event.pos)
+if __name__ == '__main__':
+    if sys.argv[1] == 'easy' or sys.argv[1] == 'medium' or sys.argv[1] == 'hard':
+        while not mouse_won() and not player_won():
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    for i in tile_group:
+                        i.check_click(event.pos)
+                elif event.type == pygame.MOUSEMOTION:
+                    for i in tile_group:
+                        i.check_hover(event.pos)
+            contor = 0
+            if not player_won() and moved[0] and blocked_tiles[0] > mouse_moves[0] and sys.argv[1] == 'hard':
+                moved[0] = False
+                mouse_moves[0] += 1
+                mouse_move_using_dijkstra()
+            elif not player_won() and moved[0] and blocked_tiles[0] > mouse_moves[0] and sys.argv[1] == 'easy':
+                moved[0] = False
+                mouse_moves[0] += 1
+                mouse_move()
+            elif not player_won() and moved[0] and blocked_tiles[0] > mouse_moves[0] and sys.argv[1] == 'medium':
+                moved[0] = False
+                mouse_moves[0] += 1
+                if contor % 2 == 1:
+                    mouse_move_using_dijkstra()
+                else:
+                    mouse_move()
+                contor += 1
 
-        if not player_won() and moved[0] and blocked_tiles[0] > mouse_moves[0]:
-            moved[0] = False
-            mouse_moves[0] += 1
-            mouse_move_using_dijkstra()
-
-        pygame.display.flip()
-        screen.blit(background, (0, 0))
-        tile_group.draw(screen)
-        tile_group.update()
-        clock.tick(60)
-    else:
-        if mouse_won():
-            print('The mouse won')
+            pygame.display.flip()
+            screen.blit(background, (0, 0))
+            tile_group.draw(screen)
+            tile_group.update()
+            clock.tick(60)
         else:
-            print('The player won')
+            if mouse_won():
+                print('The mouse won')
+            else:
+                print('The player won')
+    elif sys.argv[1] == 'human':
+        turn = [0]
+        while not mouse_won() and not player_won():
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    for i in tile_group:
+                        i.check_click_with_turn(event.pos, turn, tile_matrix)
+                elif event.type == pygame.MOUSEMOTION:
+                    for i in tile_group:
+                        i.check_hover_with_turn(event.pos, turn)
+            pygame.display.flip()
+            screen.blit(background, (0, 0))
+            tile_group.draw(screen)
+            tile_group.update()
+            clock.tick(60)
+        else:
+            if mouse_won():
+                print('The mouse player won')
+            else:
+                print('The human player won')
